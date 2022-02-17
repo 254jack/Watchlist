2416abad804abaf9e5f6ded71a60541aa55f01a4
Jack <taixonj@gmail.com>
2022-02-17 13:31:43 +0300

    add initial chages

--- app/requests.py
+++ app/requests.py
@@ -1,23 +1,23 @@
 import urllib.request,json
-from .models import movie
-
-Movie = movie.Movie
+from .models import Movie
 
 # Getting api key
 api_key = None
 # Getting the movie base url
 base_url = None
 
+
 def configure_request(app):
     global api_key,base_url
     api_key = app.config['MOVIE_API_KEY']
     base_url = app.config['MOVIE_API_BASE_URL']
 
+
 def get_movies(category):
     '''
     Function that gets the json response to our url request
     '''
-    get_movies_url = base_url.format(category,api_key)
+    get_movies_url=base_url.format(category,api_key)
 
     with urllib.request.urlopen(get_movies_url) as url:
         get_movies_data = url.read()
@@ -28,9 +28,9 @@ def get_movies(category):
         if get_movies_response['results']:
             movie_results_list = get_movies_response['results']
             movie_results = process_results(movie_results_list)
-
-
+    
     return movie_results
+
 
 def process_results(movie_list):
     '''
@@ -54,8 +54,31 @@ def process_results(movie_list):
         if poster:
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
             movie_results.append(movie_object)
+
     return movie_results
-            
+
+
+def get_movie(id):
+    get_movie_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_movie_details_url) as url:
+        movie_details_data = url.read()
+        movie_details_response = json.loads(movie_details_data)
+
+        movie_object = None
+        if movie_details_response:
+            id = movie_details_response.get('id')
+            title = movie_details_response.get('original_title')
+            overview = movie_details_response.get('overview')
+            poster = movie_details_response.get('poster_path')
+            vote_average = movie_details_response.get('vote_average')
+            vote_count = movie_details_response.get('vote_count')
+
+            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
+
+    return movie_object
+
+
 def search_movie(movie_name):
     search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
     with urllib.request.urlopen(search_movie_url) as url:
--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 export MOVIE_API_KEY = '578710306308c4f6ff754bc2268a6b4c'
-export SECRET_KEY = '12345'
+export SECRET_KEY='<Flask WTF Secret Key>'
+export MAIL_USERNAME='johnjorogek@gmail.com'
+export MAIL_PASSWORD='Ms261-0020/2010'
 
-python3.8 manage.py server
+python manage.py server
